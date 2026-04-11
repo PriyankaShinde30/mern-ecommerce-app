8ecc10b52c4405e3bdbca2c53b0ce771ab97c877
PriyankaShinde30 <priyankads3001@gmail.com>
2026-04-12 00:36:49 +0530

    Update product CRUD with search, filter, and sort

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -17,3 +17,78 @@ exports.getAllProducts = async () => {
 
     return db.execute(query);
 };
+
+exports.getProductById = async (id) => {
+    const query = `
+    SELECT 
+    p.product_id,
+    p.name,
+    p.description,
+    p.brand,
+    p.price,
+    p.discount_price,
+    p.sku,
+    c.description AS category,
+    sc.name AS sub_category
+    FROM products p 
+    JOIN categories c ON p.category_id = c.category_id
+    JOIN sub_categories sc ON p.sub_category_id = sc.sub_category_id
+    WHERE p.product_id = ?
+    `;
+
+    return db.execute(query, [id]);
+};
+
+exports.getFilteredProducts = async (filters) => {
+    let query = `
+    SELECT 
+    p.product_id, 
+    p.name,
+    p.price, 
+    p.discount_price,
+    p.brand, 
+    c.description AS category 
+    FROM products p 
+    JOIN categories c ON p.category_id = c.category_id
+    WHERE 1 = 1
+    `;
+
+    const values = [];
+
+    if(filters.search) {
+        query += ` AND p.name LIKE ?`;
+        values.push(`%${filters.search}%`);
+    }
+
+    if(filters.category) {
+        query += ` AND p.category_id = ?`;
+        values.push(filters.category);
+    }
+
+    if(filters.minPrice) {
+        query += ` AND p.price >= ?`;
+        values.push(filters.minPrice);
+    }
+
+    if(filters.maxPrice) {
+        query += ` AND p.price <= ?`;
+        values.push(filters.maxPrice);
+    }
+
+    if(filters.sort === 'price_asc') {
+        query += ` ORDER BY p.price ASC`;
+    }
+
+    if(filters.sort === 'price_desc') {
+        query += ` ORDER BY p.price DESC`;
+    }
+
+    if(filters.limit && filters.offset !== null) {
+        query += ` LIMIT ${filters.limit} OFFSET ${filters.offset}`;
+    }
+
+    console.log("Query:",query);
+    console.log("Values:", values);
+
+    return db.execute(query, values);
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const { verifyToken } = require('../middleware/authMiddleware.js');
+const { isAdmin } = require('../middleware/adminMiddlware.js');
+const upload = require('../middleware/upload');
 
 router.get('/', productController.getProducts);
+router.get('/:id', productController.getProductById);
+
+router.post('/', verifyToken, isAdmin, upload.array('images', 5), productController.addProduct);
+router.put('/:id', verifyToken, isAdmin, productController.updateProduct);
+router.delete('/:id', verifyToken, isAdmin,  productController.deleteProduct);
 
 module.exports = router;
